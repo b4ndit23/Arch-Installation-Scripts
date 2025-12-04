e3da9b2753a8c5d939617d886776f4b0c5f61de1
Nick Dandolos <128314429+b4ndit23@users.noreply.github.com>
2025-12-04 14:29:50 +0100

    Add vex-tui installation to hacking tools script

--- install-Hacking-Tools.sh
+++ install-Hacking-Tools.sh
@@ -53,6 +53,7 @@ install_packages python-pwntools python-aiosmtpd python-websocket-client python-
 
 # Go 
 go install github.com/sensepost/gowitness@latest
+go install github.com/CodeOne45/vex-tui@latest
 
 # Yay Packages
 install_packages_yay ffuf gobuster burpsuite smbmap naabu netexec ruby-evil-winrm whatweb steghide pince git-dumper-git caido-desktop rustscan hashid
